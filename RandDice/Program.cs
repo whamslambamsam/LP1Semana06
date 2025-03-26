2cea55a0b41388c05d8ac55692e48d752c0dc142
Samuel Esteves <ssamuelesteves13@gmail.com>
2025-03-26 15:33:38 +0000

    Set variable n and s

---
+++ RandDice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            n = args[0];
+            s = args[1];
+            
+        }
+    }
+}
