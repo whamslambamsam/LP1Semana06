cd4cc8a747aebe287a3b46d2a01539df4d32d54a
Samuel Esteves <ssamuelesteves13@gmail.com>
2025-03-26 14:29:06 +0000

    My first commit

---
+++ Chronos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            crono1.Start();
+            Thread.Sleep(500);
+            crono1.Stop();
+            // Get the elapsed time as a TimeSpan Value.
+            TimeSpan ts1 = crono1.Elapsed;
+        }
+    }
+}
