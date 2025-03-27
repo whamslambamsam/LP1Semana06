0e2e579e698c0f147dfe9c936b0d01d3cadb485c
Samuel Esteves <ssamuelesteves13@gmail.com>
2025-03-27 10:17:08 +0000

    Added dice roll and sum total code

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -6,9 +6,20 @@ namespace RandDice
     {
         private static void Main(string[] args)
         {
-            n = args[0];
-            s = args[1];
-            
+            int n = Int32.Parse(args[0]);
+            int s = Int32.Parse(args[1]);
+            int total = 0;
+            Random r1 = new Random(s);
+            for (int i = 0; i < n; i++)
+            {
+                int randnum = (r1.Next(1, 7));
+                total += randnum;
+            }
+
+            Console.WriteLine(total);
+
+
+
         }
     }
 }
