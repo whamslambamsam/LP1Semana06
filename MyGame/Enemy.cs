d01363df3d1ce89ab515cdbecbdde92f4e2625ef
Samuel Esteves <ssamuelesteves13@gmail.com>
2025-03-28 17:06:32 +0000

    Added public void TakeDamage method

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -15,5 +15,26 @@ namespace MyGame
         {
             return name;
         }
+        
+        public void TakeDamage(float damage)
+        {
+            // A variável shield é subtraida pelo valor da variável damage
+            shield -= damage;
+
+            // Se a variável shield for menor que zero, então:
+            if (shield < 0)
+            {
+                // Cria uma variável que guarda o valor negativo do shield
+                float DamageStillToInflict = -shield;
+                // Define shield como 0
+                shield = 0;
+                // Subtrai o valor da variável health pela variável 
+                // DamageStillToInflict
+                health -= DamageStillToInflict;
+                // Se a variável health for menor que 0, será definida de volta
+                // para 0.
+                if (health < 0) health = 0;
+            }
+        }
     }
 }
