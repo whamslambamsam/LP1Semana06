1becee63b6c5c4521759bf167215f3c0f2d08325
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-03-25 22:13:34 +0000

    Added countdown and stop

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -10,6 +10,12 @@ namespace Chronos
         {            
             Stopwatch crono1 = new Stopwatch();
             Stopwatch crono2 = new Stopwatch();
+            crono1.Start();
+            Thread.Sleep(500);
+            crono2.Start();
+            Thread.Sleep(250);
+            crono1.Stop();
+            crono2.Stop();
         }
     }
 }
