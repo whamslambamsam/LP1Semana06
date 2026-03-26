a15a0ffa6ec3ca4c8e8023f10598916a969270a8
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-03-26 15:35:08 +0000

    Expanded on Enemy class

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -4,6 +4,15 @@ namespace MyGame
 {
     public class Enemy
     {
-        
+        private string name;
+        private float health;
+        private float shield;
+
+        public Enemy(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
     }
 }
