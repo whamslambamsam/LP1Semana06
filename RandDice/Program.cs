5b233e636785f08006406b3f9567dbc81e460725
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-03-26 11:43:02 +0000

    Added RandDice project

---
+++ RandDice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
