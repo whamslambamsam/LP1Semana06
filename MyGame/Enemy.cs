6ccdd9977c4cce092754bbcd99afb6792fecd1b4
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-03-26 15:40:13 +0000

    Expanded on Enemy script further

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -8,11 +8,28 @@ namespace MyGame
         private float health;
         private float shield;
 
+        public string GetName()
+        {
+            return name;
+        }
+
         public Enemy(string name)
         {
             this.name = name;
             health = 100;
             shield = 0;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
         }
     }
 }
