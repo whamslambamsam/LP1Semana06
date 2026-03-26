31fb34a959bd66eaff57b63ad245f00731eb0424
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-03-26 12:31:45 +0000

    Added total and rand variables

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -8,6 +8,8 @@ namespace RandDice
         {
             int n = Convert.ToInt32(args[0]);
             int s = Convert.ToInt32(args[1]);
+            int total = 0;
+            Random rand = new Random(s);
 
             for(int i = 0; i < n; i++)
             {
